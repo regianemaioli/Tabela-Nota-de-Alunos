1a7b800f4b1c92f5d0cf57a7e260a866775a27c5
Regiane Maioli <regianemaioli@hotmail.com>
2020-02-23 22:06:12 -0300

    Corrigindo erro nas rotas

--- Introducao_ao_Angular_JS/js/app/controllers.js
+++ Introducao_ao_Angular_JS/js/app/controllers.js
@@ -2,23 +2,22 @@ angular.module("meuModulo")
 .controller("indexController",function($scope){
 	$scope.titulo = "Home";
 	$scope.alunos = [
-	{nome: "Camila", email: "camila@mail.com", nota1: 65, nota2: 80, nota3: 55},
-	{nome: "Pedro", email: "pedro@mail.com", nota1: 75, nota2: 80, nota3: 55},
-	{nome: "Murilo", email: "murilo@mail.com", nota1: 65, nota2: 60, nota3: 55},
-	{nome: "Joao", email: "joao@mail.com", nota1: 95, nota2: 80, nota3: 55},
-	{nome: "Ana", email: "ana@mail.com", nota1: 65, nota2: 30, nota3: 55}
+		{nome: "Camila",email:"camila@mail.com",nota1:65,nota2:80,nota3:55},
+		{nome: "Pedro",email:"pedro@mail.com",nota1:75,nota2:80,nota3:55},
+		{nome: "Murilo",email:"murilo@mail.com",nota1:65,nota2:60,nota3:55},
+		{nome: "João",email:"joao@mail.com",nota1:95,nota2:80,nota3:55},
+		{nome: "Ana",email:"ana@mail.com",nota1:65,nota2:30,nota3:55}
 	];
 
 	var init = function(){
 		$scope.alunos.forEach(function(aluno){
 			aluno.media = media(aluno);
-			
 		});
 		limpaForm();
 	};
-	var contar2 = 0;	
+	var contar2 = 0;
 	var media = function(Aluno){
-		console.log(contar2++)
+		console.log(contar2++);
 		var media = (parseFloat(Aluno.nota1) + parseFloat(Aluno.nota2) + parseFloat(Aluno.nota3))/3;
 		return media.toFixed(2);
 	};
@@ -34,17 +33,18 @@ angular.module("meuModulo")
 		$scope.alunos.push(Aluno);
 		$('#modal1').closeModal();
 		limpaForm();
-
 	};
+
 	$scope.editando = false;
 	var alunoEditar;
 
 	$scope.editarAluno = function(Aluno){
-		$scope.editando = true;
+		$scope.editando = true;			
 		angular.copy(Aluno,$scope.Aluno);
 		$('#modal1').openModal();
 		alunoEditar = Aluno;
 	};
+
 	$scope.salvarAluno = function(Aluno){
 		alunoEditar.nome = Aluno.nome;
 		alunoEditar.email = Aluno.email;
@@ -54,6 +54,7 @@ angular.module("meuModulo")
 		alunoEditar.media = media(Aluno);
 		$('#modal1').closeModal();
 	};
+
 	$scope.deletarAluno = function(Aluno){
 		for(var index in $scope.alunos){
 			var aux = $scope.alunos[index];
@@ -64,13 +65,13 @@ angular.module("meuModulo")
 	};
 
 	var limpaForm = function(){
-		$scope.Aluno = {nome:"",email:"",nota1:'',nota2:'',nota3:'',media:''};
-		
+		$scope.Aluno = {nome: "",email:"",nota1:'',nota2:'',nota3:'',media:''};
 	};
 
 	init();
+
+
 })
 .controller("contatoController",function($scope){
 	$scope.titulo = "Contato";
-
 })
